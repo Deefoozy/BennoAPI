3ffe4dbee04292ccf316120e5e2b600b7ba004ca
Deefoozy <tinotrok@outlook.com>
2024-04-28 17:50:33 +0200

    Added host to fastify listen

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ fastify.get('/test', GeneralController.Greeting);
 fastify.register(IndexRoutes.RegisterRoutes, IndexRoutes.options);
 
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({ port: 3000, host: '0.0.0.0' })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
