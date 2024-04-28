8516a5ce6295d2e893ace59a14b5518a24039009
Deefoozy <tinotrok@outlook.com>
2024-04-28 17:38:56 +0000

    Added knex initialisation to server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import "dotenv/config.js";
+import InitKnex from "knex";
 import Fastify from 'fastify';
 
 import { GeneralController } from './controllers/generalController.js';
 import { IndexRoutes } from './routes/indexRoutes.js';
+
+const knex = InitKnex({
+  client: process.env.DB_ENGINE,
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+  }
+});
 
 const fastify = Fastify({
   logger: true
